9fdc3f93e0189d38c706eb79bdc6e3e8f7a92679
fanf <fanf@fanf>
2022-05-07 11:57:45 +0200

    windows packaging

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from distutils.core import setup
 import py2exe
-data_files =  ["assets"],
+data_files =  ["assets"]
+
+data_files = [('assets', ['assets/fx13.wav','assets/fx16.wav','assets/fx22.wav'])]
+
 
 
 setup(
 
     windows=['main.py'],
-    #data_files =  data_files,
+    #zipfile="pypong.zip",
+    data_files =  data_files,
     options={
                     "py2exe":{
                             "unbuffered": True,
